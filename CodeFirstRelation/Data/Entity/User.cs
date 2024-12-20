104ffd479c59301da450488868b81ac1f454da1e
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-12-20 10:30:59 +0300

    c

---
+++ CodeFirstRelation/Data/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeFirstRelation.Data.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int UserName { get; set; }
+        public int Email { get; set; }
+
+        // Navigation Property
+        public List<Post> Posts { get; set; }
+    }
+}
--- CodeFirstRelation/Program.cs
+++ CodeFirstRelation/Program.cs
@@ -1,6 +1,11 @@
+using CodeFirstRelation.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<PatikaSecondDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
